47e22012d0d121adccc18e5a9d850fca75ff1cd0
gazlevodiva <almarka03@gmail.com>
2022-12-23 14:07:26 +0100

    set cookie after correct password, fix pass form

--- game/views/index.py
+++ game/views/index.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 
-from game.models.GameSessions import GameSessions
-from game.views.page404 import error_404
+from django.http import HttpResponseRedirect
 
+from game.models.GameSessions import GameSessions
+
+from game.views.page404 import error_404
+from game.views.new_player import new_player
 
 
 def index( request, session_hash=False ):
@@ -11,14 +14,15 @@ def index( request, session_hash=False ):
 
     if last_game_session.session_hash == session_hash:
         render_template = render(request, 'game/session_code.html')        
-        render_template.set_cookie( 'game_session_hash', session_hash )
         
         if request.POST:            
             session_code = int( request.POST['session_code'] )
             last_game_session = GameSessions.objects.latest( 'created_date' )
 
             if last_game_session.session_code == session_code:                
-                return redirect( '/new_player/' )
+                render_template = HttpResponseRedirect( '/new_player/' )       
+                render_template.set_cookie( 'game_session_hash', session_hash )
+                return render_template
         
         return render_template
 
