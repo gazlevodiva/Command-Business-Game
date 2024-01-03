20efd10c4968ea206b0cc38a67a6ff6dd587aabf
gazlevodiva <almarka03@gmail.com>
2024-01-03 09:13:30 +0100

    fix defaut Player

--- game/models/Player.py
+++ game/models/Player.py
@@ -1,5 +1,6 @@
 from django.db import models
 from game.models.GameSessions import GameSessions
+from django.utils import timezone
 
 class Player(models.Model):
     
@@ -8,7 +9,7 @@ class Player(models.Model):
     level   = models.IntegerField( default=1 )
     visible = models.BooleanField( default=True )
     game_session = models.ForeignKey( GameSessions, default=3, on_delete=models.CASCADE )
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField( default=timezone.now )
 
     def __str__(self):
         return self.name
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'player_contol', 'session_control' ],
+        'DIRS': [ 'player_control', 'session_control' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
