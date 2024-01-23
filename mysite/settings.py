f2bb8a23c7d81b2fb319510cd289e071cb5d0dbb
gazlevodiva <almarka03@gmail.com>
2024-01-23 10:46:19 +0100

    Add tax info on biz modal, fix number separator

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
@@ -130,9 +130,9 @@ STATIC_ROOT = 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# in settings.py, add: 
-USE_THOUSAND_SEPARATOR = True
-# also make sure that:
 USE_L10N = True
+USE_THOUSAND_SEPARATOR = True
+NUMBER_GROUPING = 3
+THOUSAND_SEPARATOR = ' '
 
 APPEND_SLASH = True
