133e84f0b10b6f2613316a6b59d4780e84fa6099
gazlevodiva <almarka03@gmail.com>
2024-03-14 17:53:28 +0100

    9 symb for player name!

--- game/decorators.py
+++ game/decorators.py
@@ -8,25 +8,27 @@ import re
 def check_user_session_hash(view):
     def wrapped(request, *args, **kwargs):
         try:
-
-            # Check is this player_control_ url
-            # player_control_pattern = r'^/player_control_(\d+)/$'
-            # match = re.match(player_control_pattern, request.path)
-            # if match:
-            #     player_id = int(match.group(1))
-            #     player = get_object_or_404(Player, pk=player_id)
-            #     if player:
-            #         print(f"check_user_session_hash: {player.name} - {player.id}")
-
             session_hash = request.COOKIES["game_session_hash"]
             if not session_hash:
-                print("check_user_session_hash: No session_hash. Return 404")
-                return error_404(request)
+                player_control_pattern = r'^/player_control_(\d+)/$'
+                match = re.match(player_control_pattern, request.path)
+
+                if not match:
+                    print("check_user_session_hash: No session_hash and no player. Return 404")
+                    return error_404(request)
+
+                if match:
+                    player_id = int(match.group(1))
+                    player = get_object_or_404(Player, pk=player_id)
+                    session_hash = player.game_session.session_hash
+
+                    # Need to ask session code here!!!
+                    
 
             session = GameSessions.objects.get(session_hash=session_hash)
             if not session:
                 print("check_user_session_hash: No session in DB. Return 404")
-                return error_404(request)
+                return error_404(request)            
 
         except Exception as e:
             print("check_user_session_hash: No session_hash. Return 404", e)
